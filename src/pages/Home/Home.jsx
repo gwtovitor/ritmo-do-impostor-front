20b0def020f1cd7ad4d58a7f6f13bcf8afa7be37
Vitor Augusto <108837467+gwtovitor@users.noreply.github.com>
2024-02-02 15:15:56 -0300

    home styles

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,51 +1,39 @@
 import { Musica1, Musica2, Olho1, Raio1 } from '../../lib/icons'
 import styles from './home.module.scss'
-import logo from '../../../public/logo.png'
-import io from 'socket.io-client'
-import { useEffect, useRef, useState } from 'react'
+import { useState } from 'react'
 import { toast } from 'react-toastify'
+import { useNavigate } from 'react-router-dom'
+import { Logo } from '../../components/Logo/Logo'
 
 
 export default function Home() {
     const [codigo, setCodigo] = useState('')
-    const socket = useRef(null);
-    useEffect(() => {
-        socket.current = io.connect('http://localhost:5000');
-        return () => {
-            if (socket.current) {
-                socket.current.disconnect();
-            }
-        };
-    }, []);
+    const navigate = useNavigate()
 
-    function connectSocket() {
-        if (codigo == '') {
-            toast.error('🙄 Preencha o codigo da sala', {
-                position: "top-right",
-                autoClose: 5000,
-                hideProgressBar: false,
-                closeOnClick: true,
-                pauseOnHover: true,
-                draggable: true,
-                progress: undefined,
-                theme: "dark",
-            });
+    function toLoby() {
+        if (!codigo == '') {
+            navigate(`/loby/${codigo}`)
+            return
         }
-        console.log('Connecting and emitting to "teste"');
-        if (socket.current) {
-            socket.current.emit('teste', 'vitor');
-        }
+        toast.error('🙄 Preencha o codigo da sala', {
+            position: "top-right",
+            autoClose: 5000,
+            hideProgressBar: false,
+            closeOnClick: true,
+            pauseOnHover: true,
+            draggable: true,
+            progress: undefined,
+            theme: "dark",
+        });
     }
     return (
         <div className={styles.homeWrapper}>
             <div className={styles.home}>
-                <div className={styles.logo}>
-                    <img src={logo} />
-                </div>
+                <Logo />
                 <div className={styles.buttonsWrapper}>
-                    <a onClick={() => { connectSocket() }}>Criar uma Sala</a>
+                    <a onClick={() => { toLoby() }}>Criar uma Sala</a>
                     <input onChange={(e) => { setCodigo(e.target.value) }} placeholder='CODIGO DA SALA'></input>
-                    <a onClick={() => { connectSocket() }}>Entrar em uma Sala</a>
+                    <a>Entrar em uma Sala</a>
                 </div>
 
                 <Icons />
